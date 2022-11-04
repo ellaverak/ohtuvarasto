c4ad7a6d76ede49402e3c7d22ffd758f85333fe6
ellaverak <ella.korkeaaho@gmail.com>
2022-11-04 15:25:43 +0200

    add tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -1,22 +1,38 @@
 import unittest
+from unittest.mock import patch
 from varasto import Varasto
+import io
 
 
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.varasto_0 = Varasto(-10)
+        self.varasto_01 = Varasto(5, -5)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.assertAlmostEqual(self.varasto_0.saldo, -10)
+        self.assertAlmostEqual(self.varasto_01.saldo, 0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+        self.assertAlmostEqual(self.varasto_0.tilavuus, 0)
+        self.assertAlmostEqual(self.varasto_01.tilavuus, 5)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+        self.varasto.lisaa_varastoon(-8)
+
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -31,6 +47,16 @@ class TestVarasto(unittest.TestCase):
 
         self.assertAlmostEqual(saatu_maara, 2)
 
+        saatu_maara = self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+        saldo = self.varasto.saldo
+
+        saatu_maara = self.varasto.ota_varastosta(100)
+
+        self.assertAlmostEqual(saatu_maara, saldo)
+
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +64,8 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_str(self, mock_stdout):
+        print(self.varasto)
+        assert mock_stdout.getvalue() == f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}\n"
